859f0133fc32fa9fd3bbfeec66b1514e6000202b
lcameron05 <lcameron05@gmail.com>
2016-09-14 12:15:17 +0100

    SinglePathManager unittest added

--- relative_path_test.py
+++ relative_path_test.py
@@ -1,6 +1,38 @@
 from pcwg.configuration.path_manager import PathManager
+from pcwg.configuration.path_manager import SinglePathManager
 import unittest
 
+class TestSinglePathManager(unittest.TestCase):
+    
+    def test_one(self):
+        
+        path_manager = SinglePathManager()
+
+        analysis_path = r"C:\Data\analysis.xml"  
+        
+        power_path_rel = r"PowerCurves\PowerCurves.xml"   
+        power_path_abs = r"C:\Data\PowerCurves\PowerCurves.xml"   
+
+        self.assertTrue(path_manager.absolute_path is None)
+        self.assertTrue(path_manager.relative_path is None)
+        self.assertTrue(path_manager.display_path is "")
+        
+        path_manager.set_base(analysis_path)
+
+        self.assertEqual(path_manager.base_folder, "C:\Data")
+
+        self.assertTrue(path_manager.absolute_path is None)
+        self.assertTrue(path_manager.relative_path is None)
+        self.assertTrue(path_manager.display_path is "")
+        
+        path_manager.relative_path = power_path_rel
+        
+        print path_manager.absolute_path
+        
+        self.assertTrue(path_manager.absolute_path == power_path_abs)
+        self.assertTrue(path_manager.relative_path == power_path_rel)
+        self.assertTrue(path_manager.display_path == path_manager.relative_path)
+        
 class TestPathManager(unittest.TestCase):
     
     def test_one(self):
