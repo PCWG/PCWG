28dd6e7ab15a7ca7cf8ae93f0768cfa125577d2b
peterdougstuart <peter.stuart@gmail.com>
2018-05-08 22:37:35 +0100

    updated version number, new help file, updated tests to fix build

--- tests/test_gui_share_x.py
+++ tests/test_gui_share_x.py
@@ -43,62 +43,70 @@ class TestUserInterfaceShareX:
             assert_in(file_name, files_in_zip)
 
     def test_share_1(self):
+
         xls_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='01', ext='xls'))
         zip_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='01', ext='zip'))
+
         summary_file = 'Summary.xls'
 
         self.mock_app.PCWG_Share_1_Portfolio()
 
         assert_true(os.path.isfile(zip_out))
         self.check_zip_file_contains_n_files(zip_out, 3)
-        self.check_zip_file_contains_file_of_name(zip_out, summary_file)
+        self.check_zip_file_contains_file_of_name(zip_out, xls_out)
 
         zip_out.extract(path=xls_out, member=summary_file)
         assert_true(os.path.isfile(xls_out))
         self.check_output_xls_file_has_n_valid_results(xls_out, 2)
 
     def test_share_1_dot_1(self):
-        xls_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='01.1', ext='xls'))
+
         zip_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='01.1', ext='zip'))
-        summary_file = 'Summary.xls'
+
+        xls_out_file = self.output_file_name.format(share_num='01.1', ext='xls')
+        xls_out = os.path.join(FILE_DIR, 'data', xls_out_file)
 
         self.mock_app.PCWG_Share_1_dot_1_Portfolio()
 
         assert_true(os.path.isfile(zip_out))
         self.check_zip_file_contains_n_files(zip_out, 3)
-        self.check_zip_file_contains_file_of_name(zip_out, summary_file)
+        self.check_zip_file_contains_file_of_name(zip_out, xls_out_file)
 
-        zip_out.extract(path=xls_out, member=summary_file)
+        zip_out.extract(path=xls_out, member=xls_out_file)
         assert_true(os.path.isfile(xls_out))
         self.check_output_xls_file_has_n_valid_results(xls_out, 2)
 
     def test_share_2(self):
-        xls_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='02', ext='xls'))
+
         zip_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='02', ext='zip'))
 
-        summary_file = 'Summary.xls'
+        xls_out_file = self.output_file_name.format(share_num='02', ext='xls')
+        xls_out = os.path.join(FILE_DIR, 'data', xls_out_file)
+
         self.mock_app.PCWG_Share_2_Portfolio()
 
         assert_true(os.path.isfile(zip_out))
         self.check_zip_file_contains_n_files(zip_out, 3)
-        self.check_zip_file_contains_file_of_name(zip_out, summary_file)
+        self.check_zip_file_contains_file_of_name(zip_out, xls_out_file)
 
-        zip_out.extract(path=xls_out, member=summary_file)
+        zip_out.extract(path=xls_out, member=xls_out_file)
         self.check_output_xls_file_has_n_valid_results(xls_out, 2)
         assert_true(os.path.isfile(xls_out))
 
     def test_share_3(self):
-        xls_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='03', ext='xls'))
+
         zip_out = os.path.join(FILE_DIR, 'data', self.output_file_name.format(share_num='03', ext='zip'))
 
-        summary_file = 'Summary.xls'
+        xls_out_file = self.output_file_name.format(share_num='03', ext='xls')
+        xls_out = os.path.join(FILE_DIR, 'data', xls_out_file)
+
         self.mock_app.PCWG_Share_3_Portfolio()
 
         assert_true(os.path.isfile(zip_out))
         self.check_zip_file_contains_n_files(zip_out, 3)
-        self.check_zip_file_contains_file_of_name(zip_out, summary_file)
+        self.check_zip_file_contains_file_of_name(zip_out, xls_out_file)
 
-        zip_out.extract(path=xls_out, member=summary_file)
+        zip_out.extract(path=xls_out, member=xls_out_file)
         self.check_output_xls_file_has_n_valid_results(xls_out, 2)
         assert_true(os.path.isfile(xls_out))
 
--- version.py
+++ version.py
@@ -5,4 +5,4 @@ Created on Wed Aug 10 14:55:16 2016
 @author: Stuart
 """
 
-version = "0.8.0-Beta"
+version = "0.8.0"
